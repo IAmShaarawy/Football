4b163d735a458b90d1f15e42713169017eb0fe1e
Mohamed Elshaarawy <elshaarawy200825@hotmail.com>
2018-07-25 01:29:11 +0200

    init team activity

---
+++ app/src/main/java/net/elshaarawy/football/team/TeamActivity.kt
@@ -0,0 +1,13 @@
+package net.elshaarawy.football.team
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import net.elshaarawy.football.R
+
+class TeamActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_team)
+    }
+}
