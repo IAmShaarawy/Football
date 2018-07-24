0d28147701437017bf0dc5f5d8f71e8b3b3b800c
Mohamed Elshaarawy <elshaarawy200825@hotmail.com>
2018-07-25 01:27:50 +0200

    init teams activity

--- app/src/main/java/net/elshaarawy/football/teams/TeamsActivity.kt
+++ app/src/main/java/net/elshaarawy/football/teams/TeamsActivity.kt
@@ -2,6 +2,7 @@ package net.elshaarawy.football.teams
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import net.elshaarawy.football.R
 
 class TeamsActivity : AppCompatActivity() {
 
