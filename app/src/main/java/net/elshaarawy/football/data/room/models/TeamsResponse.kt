3dc1d5a61a381642322ca4df4f0073d22d71846d
Mohamed Elshaarawy <elshaarawy200825@hotmail.com>
2018-07-25 06:10:29 +0200

    feat: create Teams Response

---
+++ app/src/main/java/net/elshaarawy/football/data/room/models/TeamsResponse.kt
@@ -0,0 +1,12 @@
+package net.elshaarawy.football.data.room.models
+
+import com.google.gson.annotations.SerializedName
+import net.elshaarawy.football.data.room.entities.TeamEntity
+
+/**
+ * Created by elshaarawy on 7/25/18.
+ */
+data class TeamsResponse(
+        @SerializedName("teams")
+        val teamsList: List<TeamEntity>
+)
