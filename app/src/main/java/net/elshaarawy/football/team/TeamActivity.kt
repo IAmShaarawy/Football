e1d6bd0579a31158b1f3d0dc36b903486a2d74f7
Mohamed Elshaarawy <elshaarawy200825@hotmail.com>
2018-07-25 12:07:36 +0200

    feat: initiate viewmodel instances using thier factories

--- app/src/main/java/net/elshaarawy/football/team/TeamActivity.kt
+++ app/src/main/java/net/elshaarawy/football/team/TeamActivity.kt
@@ -1,13 +1,20 @@
 package net.elshaarawy.football.team
 
+import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import net.elshaarawy.football.R
 
 class TeamActivity : AppCompatActivity() {
 
+    lateinit var teamViewModel: TeamViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_team)
+
+        val teamId: Long = 5
+        teamViewModel = ViewModelProviders.of(this, TeamViewModelFactory(teamId))
+                .get(TeamViewModel::class.java)
     }
 }
--- app/src/main/java/net/elshaarawy/football/teams/TeamsActivity.kt
+++ app/src/main/java/net/elshaarawy/football/teams/TeamsActivity.kt
@@ -1,13 +1,19 @@
 package net.elshaarawy.football.teams
 
+import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import net.elshaarawy.football.R
 
 class TeamsActivity : AppCompatActivity() {
 
+    lateinit var teamsViewModel: TeamsViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_teams)
+
+        val teamId: Long = 5
+        teamsViewModel = ViewModelProviders.of(this, TeamsViewModelFactory(teamId))
+                .get(TeamsViewModel::class.java)
     }
 }
