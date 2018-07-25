5bddb0092aecd155f8f225a0cdfadce0c4c6cbed
Mohamed Elshaarawy <elshaarawy200825@hotmail.com>
2018-07-25 06:11:07 +0200

    refactor: delete Leagues Response Kotlin Class

--- app/src/main/java/net/elshaarawy/football/data/room/models/LeaguesResponse.kt
+++
@@ -1,8 +0,0 @@
-package net.elshaarawy.football.data.room.models
-
-import net.elshaarawy.football.data.room.entities.LeagueEntity
-
-/**
- * Created by elshaarawy on 7/25/18.
- */
-data class LeaguesResponse(val leaguesList: List<LeagueEntity>)
