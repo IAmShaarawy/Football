cc4b132569ddec33370eb7c13c76fe6561e25379
Mohamed Elshaarawy <elshaarawy200825@hotmail.com>
2018-07-25 07:50:17 +0200

    feat: implement football apis and create an object from it

---
+++ app/src/main/java/net/elshaarawy/football/data/networking/FootballApis.kt
@@ -0,0 +1,27 @@
+package net.elshaarawy.football.data.networking
+
+import io.reactivex.Observable
+import net.elshaarawy.football.data.room.entities.LeagueEntity
+import net.elshaarawy.football.data.room.models.FixturesResponse
+import net.elshaarawy.football.data.room.models.TeamsResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+
+/**
+ * Created by elshaarawy on 7/25/18.
+ */
+interface FootballApis {
+    @GET("competitions")
+    fun getLeagues(): Observable<List<LeagueEntity>>
+
+    @GET("competitions/{league_id}/teams")
+    fun getLeagueTeams(@Path("league_id") leagueId: Long): Observable<TeamsResponse>
+
+    @GET("teams/{team_id}/fixtures")
+    fun getTeamFixtures(@Path("team_id") teamId: Long): Observable<FixturesResponse>
+}
+
+object footballApis{
+    val apis = retrofitManager.networkCall.create(FootballApis::class.java)
+}
