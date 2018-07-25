11f280c003524506272902f838d03009520c027d
Mohamed Elshaarawy <elshaarawy200825@hotmail.com>
2018-07-25 15:14:09 +0200

    feat: swapping to refresh is added

--- app/src/main/java/net/elshaarawy/football/leagues/LeaguesActivity.kt
+++ app/src/main/java/net/elshaarawy/football/leagues/LeaguesActivity.kt
@@ -5,6 +5,7 @@ import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
+import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.activity_leagues.*
 import net.elshaarawy.football.R
 
@@ -12,27 +13,42 @@ class LeaguesActivity : AppCompatActivity() {
 
     lateinit var leaguesViewModel: LeaguesViewModel
     val leaguesAdapter by lazy { LeaguesAdapter() }
+    var isFirstTime = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_leagues)
+
         leaguesViewModel = ViewModelProviders.of(this).get(LeaguesViewModel::class.java)
 
-
-        leaguesViewModel.loadData()
+        leaguesViewModel.listData
                 .observe(this, Observer(leaguesAdapter::onDataChange))
+        leaguesViewModel.loadingSubject
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { league_refresh.isRefreshing = it }
 
-        if (league_rv != null) {
-            league_rv.let {
-                it.layoutManager = GridLayoutManager(this, 1)
-                it.adapter = leaguesAdapter
-            }
+        league_refresh.setOnRefreshListener { leaguesViewModel.loadData() }
 
-        } else {
-            league_rv_land.let {
-                it.layoutManager = GridLayoutManager(this, 2)
-                it.adapter = leaguesAdapter
-            }
+        isFirstTime.takeIf { true }
+                .apply { leaguesViewModel.loadData() }
+                .also { isFirstTime = false }
+
+
+        if (league_rv != null) bindPortrait() else bindLandscape()
+    }
+
+    private fun bindPortrait() {
+        league_rv.let {
+            it.layoutManager = GridLayoutManager(this, 1)
+            it.adapter = leaguesAdapter
+        }
+    }
+
+    private fun bindLandscape() {
+        league_rv_land.let {
+            it.layoutManager = GridLayoutManager(this, 2)
+            it.adapter = leaguesAdapter
         }
     }
 }
