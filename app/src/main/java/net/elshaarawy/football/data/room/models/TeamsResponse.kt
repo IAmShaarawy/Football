970aaa552ea89598e2d58776142e7d5026f2f4d0
Mohamed Elshaarawy <elshaarawy200825@hotmail.com>
2018-07-25 06:14:28 +0200

    refactro: formate Teams Response Class

--- app/src/main/java/net/elshaarawy/football/data/room/models/TeamsResponse.kt
+++ app/src/main/java/net/elshaarawy/football/data/room/models/TeamsResponse.kt
@@ -6,7 +6,4 @@ import net.elshaarawy.football.data.room.entities.TeamEntity
 /**
  * Created by elshaarawy on 7/25/18.
  */
-data class TeamsResponse(
-        @SerializedName("teams")
-        val teamsList: List<TeamEntity>
-)
+data class TeamsResponse(@SerializedName("teams") val teamsList: List<TeamEntity>)
