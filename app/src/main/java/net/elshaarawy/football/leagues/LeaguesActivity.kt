402336f87869e15cd10a8a8ee983df6642871760
Mohamed Elshaarawy <elshaarawy200825@hotmail.com>
2018-07-25 13:47:32 +0200

    feat: finish ui design for league screen

--- app/src/main/java/net/elshaarawy/football/leagues/LeaguesActivity.kt
+++ app/src/main/java/net/elshaarawy/football/leagues/LeaguesActivity.kt
@@ -3,8 +3,9 @@ package net.elshaarawy.football.leagues
 import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_leagues.*
 import net.elshaarawy.football.R
-import net.elshaarawy.football.data.room.FootballDatabase
 
 class LeaguesActivity : AppCompatActivity() {
 
@@ -14,5 +15,11 @@ class LeaguesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_leagues)
         leaguesViewModel = ViewModelProviders.of(this).get(LeaguesViewModel::class.java)
+
+        if (league_rv != null) {
+            league_rv.layoutManager = GridLayoutManager(this, 1)
+        } else {
+            league_rv_land.layoutManager = GridLayoutManager(this, 2)
+        }
     }
 }
